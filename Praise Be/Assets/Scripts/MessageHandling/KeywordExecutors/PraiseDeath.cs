b3b6c332a8a24140fb0b0de11ed69da8c49b77bc
Michael Vittiglio <michael.vittiglio@gmail.com>
2016-01-30 17:49:11 -0500

    Message handling, generation (including Twitch), and basic parsing integrated and demostrated (via Scene).

---
+++ Praise Be/Assets/Scripts/MessageHandling/KeywordExecutors/PraiseDeath.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using UnityEngine;
+using System.Collections;
+
+public class PraiseDeath : GameExecutor {
+    override public void Execute(Executor.Message message) {
+        this.LogDebug(message.from + " praises DEATH! (" + message.message + ")");
+    }
+}
