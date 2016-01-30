1a9dd0a6204f128cf49be2cc04327229dea137bb
Joshua Johnson <johnson.joshua.work@gmail.com>
2016-01-30 15:13:33 -0500

    oops, uninitialized game state also peeple

---
+++ Praise Be/Assets/Scripts/Person.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Person : MonoBehaviour {
+
+    public Image personImage;
+    public float moveChancePct;
+    
+	// Use this for initialization
+	void Start () {
+        moveChancePct = 0.3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Random.Range(0,1) > moveChancePct)
+        {
+            MoveRandom();
+        }
+	}
+
+    private void MoveRandom()
+    {
+        switch (Random.Range(0,4))
+        {
+            case 0:
+                // Move up
+                break;
+            case 1:
+                // Move down
+                break;
+            case 2:
+                // Move left
+                break;
+            case 3:
+                // Move right
+                break;
+        }
+    }
+}
