5ec6e0c4fe27a73b709a895a6f8f11bb8a8644ae
Joshua Johnson <johnson.joshua.work@gmail.com>
2016-01-30 14:42:59 -0500

    Rounds and tracking

---
+++ Praise Be/Assets/Scripts/StatGameState.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatGameState : MonoBehaviour {
+
+    // So this should actually hold the game state
+    // Moving to a new object to better support history
+
+    // Stats based directly on user input
+    // Faith
+    public double faithValue;
+    // Viewer count - being optimistic ;)
+    public long viewerCount;
+
+    // So 3 gods, to start just a raw stat for each
+    public double lifeGodValue;
+    public double deathGodValue;
+    public double chaosGodValue;
+
+    // Rando
+    public double anarchyValue;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Clone bitches
+    /// </summary>
+    /// <returns></returns>
+    public StatGameState Clone()
+    {
+        return (StatGameState)MemberwiseClone();
+    }
+}
--- Praise Be/Assets/Scripts/StatTracker.cs
+++ Praise Be/Assets/Scripts/StatTracker.cs
@@ -1,29 +1,54 @@
 using UnityEngine;
 using System.Collections;
 using Assets.Scripts;
+using System.Collections.Generic;
 
 public class StatTracker : Singleton<StatTracker> {
 
-    // Stats:
-    // Doubles probably make the most sense
+    // How many of these can we hold?
+    // Fuck it, start with 256
+    const int maxHistoryLength = 256;
 
-    // Stats based directly on user input
-    // Faith
-    public double faithValue;
-    // Viewer count - being optimistic ;)
-    public long viewerCount;
+    // We should have at least a brief history of previous gamestates (rounds)
+    private Queue<StatGameState> history;
+    // Current state, will be cloned and pushed into history on round end
+    private StatGameState currentState;
 
-    // So 3 gods, to start just a raw stat for each
-    public double lifeGodValue;
-    public double deathGodValue;
-    public double chaosGodValue;
-
-    // Rando
-    public float anarchyValue;
+    // Accessors for current game state
+    public double currentFaithValue
+    {
+        get { return currentState.faithValue; }
+        set { currentState.faithValue = value; }
+    }
+    public long currentViewerCount
+    {
+        get { return currentState.viewerCount; }
+        set { currentState.viewerCount = value; }
+    }
+    public double currentLifeGodValue
+    {
+        get { return currentState.lifeGodValue; }
+        set { currentState.lifeGodValue = value; }
+    }
+    public double currentDeathGodValue
+    {
+        get { return currentState.deathGodValue; }
+        set { currentState.deathGodValue = value; }
+    }
+    public double currentChaosGodValue
+    {
+        get { return currentState.chaosGodValue; }
+        set { currentState.chaosGodValue = value; }
+    }
+    public double currentAnarchyValue
+    {
+        get { return currentState.anarchyValue; }
+        set { currentState.anarchyValue = value; }
+    }
 
     // Use this for initialization
     void Start () {
-	
+        history = new Queue<StatGameState>();
 	}
 	
 	// Update is called once per frame
@@ -31,5 +56,18 @@ public class StatTracker : Singleton<StatTracker> {
 
 	}
 
+    // This will get fired on a timer, this timer may come from this class
+    // Or may come from the parser or the simulation
+    public void EndRound()
+    {
+        // Invoke the simulation? or is simulation calling this?
+        // At the very least push it to history
+        history.Enqueue(currentState.Clone());
 
+        if (history.Count > maxHistoryLength)
+        {
+            // Don't even care
+            history.Dequeue();
+        }
+    }
 }
