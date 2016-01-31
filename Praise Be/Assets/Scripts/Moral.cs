33d4dad37a737dac90a3ff2b3c8600a1cb4f01b6
Keith Morgado <keith.morgado@gmail.com>
2016-01-30 23:10:14 -0500

    New font. New GUI elements

--- Praise Be/Assets/Scripts/Moral.cs
+++ Praise Be/Assets/Scripts/Moral.cs
@@ -2,28 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
+using UnityEngine.UI;
 
 public class Moral : MonoBehaviour {
 
 
-    static bool shouldShowMoral = true;
+    static bool shouldShowMoral = false;
+    static bool newMoral = false;
 
     static Timer _timer;
     static Timer _Moraltimer;
 
     public GameObject parentObject;
+    public Text moralQuestion;
+    public Text moralTimerText;
+
+    public int numberOfMinPerQuandry = 1;
 
 
-    public int numberOfMinPerQuandry = 2;
+    private List<string> MoralQuandries = new List<string>();
 
 
 	// Use this for initialization
 	void Start () {
 
-        _timer = new Timer(numberOfMinPerQuandry * 60 * 1000);
-        _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
-        _timer.Enabled = true; // Enable it
 
+        MoralQuandries.Add("You’re headed to an important meeting when you see a nearsighted old lady struggling to cross the street. Do you stop and help, even though it might make you late?");
+        MoralQuandries.Add("You’re about to get your 8-year-old daughter the toy chemistry set she’s wanted for weeks, but at the store, she suddenly decides she wants a big stuffed bear instead. Do you buy her the bear?");
+        MoralQuandries.Add("There is a button and if you push it there is a 95% chance all suffering on earth will instantly cease and we would live in a utopian world but there is a 5% chance everyone in earth will die painfully? Do you hit the button?");
+        MoralQuandries.Add("Your best friend commits a serious felony, and you know they had no justification. Do you turn in your friend?");
+        MoralQuandries.Add("You're a doctor. You've pledged an oath to save lives. The man who killed your daughter was in a horrible car accident and is going to die without medical attention. You are the only one who can provide it. Will you save this man?");
+        MoralQuandries.Add("You have cheated Death. You are told that you have to kill someone. You either kill someone you love dearly, and NO ONE will ever know you did it. OR you must publicly execute a complete stranger, whom Death chooses. Do you kill the stanger?");
+        //parentObject.SetActive(false);
+
+        //numberOfMinPerQuandry * 60 * 1000
+        _timer = new Timer(5000);
+        _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+        _timer.AutoReset = true;
+        _timer.Enabled = true; // Enable it
 
         //Morals will last for 30 seconds
         _Moraltimer = new Timer(30000); 
@@ -33,6 +49,14 @@ public class Moral : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        moralTimerText.text = _Moraltimer.ToString();
+
+        if (newMoral)
+        {
+            moralQuestion.text = MoralQuandries[Random.Range(0, MoralQuandries.Count)];
+            newMoral = false;
+        }
 
         if (shouldShowMoral)
         {
@@ -49,15 +73,15 @@ public class Moral : MonoBehaviour {
     static void _timer_Elapsed(object sender, ElapsedEventArgs e)
     {
         shouldShowMoral = true;
+        newMoral = true;
         _Moraltimer.Enabled = true;
-        _timer.Stop();
-
+        //_timer.Stop();
     }
 
     static void _moralTimer_Elapsed(object sender, ElapsedEventArgs e)
     {
         shouldShowMoral = false;
-        _timer.Start();
+        //_timer.Start();
     }
     
 }
